0826b03643a8ee377089680f247b98f12cc91bd4
Rumbidzai <maryruemuzenda@gmail.com>
2023-02-18 18:20:48 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - Prints "with proper grammar, but the ouptu is a piece of art,"
+* main - A program that prints a line using the printf function
 *
 * Return: Always 0 (Success)
 */
