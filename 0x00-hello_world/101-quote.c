2afdcfffb14f372e26ffe14b01d3adc7a8b4386a
Rumbidzai <maryruemuzenda@gmail.com>
2023-02-20 10:07:26 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- *
+ * main - A C program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5\9);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
