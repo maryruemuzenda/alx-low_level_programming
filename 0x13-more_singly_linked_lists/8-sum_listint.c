c0a2159c24081b0c30ac3f5727a3163ba7366317
Rumbidzai <maryruemuzenda@gmail.com>
2023-04-04 18:06:50 -0700

    Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - the nth node of a listint_t linked list.
- * @head: pointer of the first node
- * @index: index is the index of the node
+ * sum_listint - sum of all the data (n) of a listint_t linked list.
+ * @head: pointer to the first node
  *
- * Return: pointer of Index
+ * Return: sum all the data
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int sum_listint(listint_t *head)
 {
-	unsigned int i;
+	int sum = 0;
 
-	if (head == NULL)
-		return (NULL);
-	for (i = 0; i < index; i++)
+	while (head != NULL)
 	{
+		sum += head->n;
 		head = head->next;
-		if (head == NULL)
-			return (NULL);
 	}
-	return (head);
+	return (sum);
 }
