72c78ad0264d815147246dc0057d1cb5ca9f5d03
Rumbidzai <maryruemuzenda@gmail.com>
2023-04-09 21:02:16 -0700

    Binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,9 +16,9 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		j = 2 * j + (b[i] - '0');
+		j = 2 * j + (b[i] -'0');
 	}
 	return (j);
 }
