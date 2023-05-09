9309f77ac4d95583c91d9e951cc01963f5420bf6
Rumbidzai <maryruemuzenda@gmail.com>
2023-05-09 12:50:17 -0700

    Binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,16 +2,16 @@
 
 /**
  * binary_to_uint -converts a binary number to an unsigned int.
- * @b: string containing the binary number
+ * @b: pointing to a string of 0 and 1 chars
  *
  * Return:converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int k;
+	unsigned int j;
 
-	k = 0;
+	j = 0;
 	if (!b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
@@ -21,9 +21,9 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		k <<= 1;
+		j <<= 1;
 		if (b[i] == '1')
-			k = 1;
+			j += 1;
 	}
-	return (k);
+	return (j);
 }
